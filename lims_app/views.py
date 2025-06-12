949222fd41a2dbeae7cb4ecea34f2b90d9862daa
Bennymens <Benedictmensah235@yahoo.com>
2025-06-11 19:03:07 -0700

    Add reader registration and search functionality to readers page

--- lims_app/views.py
+++ lims_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import reader  # use the correct model name (lowercase 'r')
+from django.shortcuts import render, redirect
+from .models import reader
+from django.db.models import Q
 
 def home(request):
     return render(request, 'home.html', {
@@ -9,9 +9,29 @@ def home(request):
     })
 
 def readers(request):
-    readers_list = reader.objects.all()
+    query = request.GET.get('q', '')
+    if query:
+        readers_list = reader.objects.filter(
+            Q(reader_name__icontains=query) |
+            Q(reader_email__icontains=query) |
+            Q(reference_id__icontains=query)
+        )
+    else:
+        readers_list = reader.objects.all()
+
+    if request.method == 'POST':
+        reader.objects.create(
+            reader_name=request.POST['reader_name'],
+            reader_email=request.POST['reader_email'],
+            reference_id=request.POST['reference_id'],
+            reader_address=request.POST['reader_address'],
+            active=True
+        )
+        return redirect('readers')
+
     return render(request, 'readers.html', {
         "title": "Readers",
         "current_tab": "readers",
-        "readers": readers_list
+        "readers": readers_list,
+        "query": query,
     })
